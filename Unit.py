c85c969345c0b1f6d504036de513228ad11e0dc2
waranya1999s <waranya.1999s@gmail.com>
2025-08-16 17:46:35 +0700

    Add files via upload

---
+++ Unit.py
@@ -0,0 +1,96 @@
+import streamlit as st
+
+import streamlit as st
+import requests
+from pathlib import Path
+
+API_URL = "https://api-voice.botnoi.ai/openapi/v1/generate_audio"
+API_TOKEN = "piJ5bwPRpWRKWZ0M2oqJp2gFdG01Zt9Q"
+
+st.set_page_config(page_title='Body Mass Index : Web Application',page_icon='😎')
+
+st.title('ค่า BMI')
+st.snow()
+st.image('bmi1.jpg')
+kg=st.number_input('นํ้าหนัก (Kg):')
+cm=st.number_input('ส่วนสูง (Cm):')
+
+from gtts import gTTS
+import io
+if st.button('คำนวณ'):
+    bmi=kg/(cm/100)**2
+    tt = f'ค่า BMI ของคุณคือ {bmi:.2f}'
+    if bmi < 18.50:
+        st.info(tt)
+        st.image('image1.PNG')
+        word='ผอมเกินไป'
+
+    elif bmi < 24.9:
+        st.success(tt)
+        st.image('image2.PNG')
+        word='ปกติ'
+        
+    elif bmi < 29.90:
+        st.warning(tt)
+        st.image('image3.PNG')
+        
+    else:
+        st.error(tt)
+
+
+    payload = {
+        "text": word,
+        "speaker": "1",
+        "volume": 1,
+        "speed": 1,
+        "type_media": "mp3",
+        "save_file": "true",
+        "language": "th",
+        "page": "user"
+    }
+
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json",
+        "botnoi-token": API_TOKEN
+    }
+
+    try:
+        res = requests.post(API_URL, json=payload, headers=headers, timeout=30)
+        res.raise_for_status()
+        data = res.json()
+        st.write("API Response:", data)
+
+        # ดึง URL ไฟล์เสียง
+        audio_url = (
+            data.get("url")
+            or data.get("audio_url")
+            or (data.get("data") or {}).get("url")
+        )
+
+        if audio_url:
+            audio_bytes = requests.get(audio_url, timeout=30).content
+            out_path = Path("botnoi_voice.mp3")
+            out_path.write_bytes(audio_bytes)
+            st.success(f"✅ บันทึกเสียงเรียบร้อย → {out_path.resolve()}")
+            st.audio(audio_bytes, format="audio/mp3")
+        else:
+            st.error("ไม่พบลิงก์ไฟล์เสียงใน response")
+
+    except Exception as e:
+        st.error(f"เกิดข้อผิดพลาด: {e}")
+
+
+
+
+
+        
+col1,col2 = st.columns(2)
+with col1:
+    if st.button('ฟังเพลง'):
+        st.video('https://youtu.be/wn0IyvGBeUI?si=MPGe3oAYYWyemaoi')
+    
+with col2:
+    if st.button('vlog'):
+        st.video('https://youtu.be/1BJvsUG9wIA?si=IkLZYv199N7IPO9Q')
+
