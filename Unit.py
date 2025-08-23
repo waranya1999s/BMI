fda5b289f36c50edab11327fbfe66f1f53017275
waranya1999s <waranya.1999s@gmail.com>
2025-08-23 18:57:36 +0700

    Update Unit.py

--- Unit.py
+++ Unit.py
@@ -1,7 +1,7 @@
 def testAi(question):
     from openai import OpenAI
 
-    client = OpenAI(api_key="")
+    client = OpenAI(api_key="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")
 
     response = client.chat.completions.create(
         model="gpt-4o-mini",  
